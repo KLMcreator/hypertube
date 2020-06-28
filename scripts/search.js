68b356c6adc73b398c36e21425693c98cb674348
Clément VANNICATTE <klm@clements-mac-mini.home>
2020-06-29 01:15:24 +0200

    fuck

--- scripts/search.js
+++ scripts/search.js
@@ -335,44 +335,6 @@ const getImages = async () => {
   }
 };
 
-const resizeDirectoryImages = async () => {
-  return new Promise((resolve, reject) => {
-    glob(
-      "*.@(png|jpg|bmp)",
-      {
-        nocase: true,
-        nodir: true,
-        realpath: true,
-        cwd: "./../client/src/assets/torrents",
-      },
-      (err, files) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(files);
-        }
-      }
-    );
-  })
-    .then((files) => {
-      let i = 0;
-      movies = [];
-      while (i < 1000) {
-        movies.push(files[i]);
-        i++;
-      }
-      return Promise.all(
-        movies.map(async (path) => {
-          const image = await jimp.read(path);
-          await image.resize(160, 240);
-          await image.quality(90);
-          await image.writeAsync(path);
-        })
-      );
-    })
-    .catch((err) => console.log(i, err));
-};
-
 const initScraping = async () => {
   console.time("initScraping");
   //   console.log("Starting new scrap at", chalk.yellow(moment().format()));
@@ -388,7 +350,6 @@ const initScraping = async () => {
   //   await saveToFile();
   //   await groupAndSave();
   //   await getImages();
-  await resizeDirectoryImages().then(console.log("ALL DONE!"));
   console.log(
     "If the process isn't finished yet, there might be some images that are still being saved. Please don't quit either way you won't have the full image bank."
   );
